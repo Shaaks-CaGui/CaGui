df6bdb16095ec2938ea5b777668c17ad8a27bfcd
Aditya Gupta <ashishgupta9936ad@gmail.com>
2022-07-06 19:12:38 +0530

    Dashboard navBar

--- app/src/main/java/com/sparklead/cagui/ui/activities/DashboardActivity.kt
+++ app/src/main/java/com/sparklead/cagui/ui/activities/DashboardActivity.kt
@@ -1,30 +1,59 @@
 package com.sparklead.cagui.ui.activities
 
+import android.app.PendingIntent.getActivity
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.fragment.app.FragmentManager
 import com.sparklead.cagui.R
+import com.sparklead.cagui.ui.fragments.AdmissionFragment
+import com.sparklead.cagui.ui.fragments.ExploreFragment
+import com.sparklead.cagui.ui.fragments.FAQsFragment
+import com.sparklead.cagui.ui.fragments.HomeFragment
+import kotlinx.android.synthetic.main.activity_dashboard.*
 
 class DashboardActivity : AppCompatActivity() {
+
+    private val fragment = HomeFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
+        openMainFragment()
+        supportActionBar?.hide()
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_dasboard)
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_explore, R.id.navigation_admissions,R.id.navigation_faqs
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        nav_view.setItemSelected(R.id.navigation_home)
+
+        nav_view.setOnItemSelectedListener {
+            when (it) {
+
+                R.id.navigation_home -> {
+                    openMainFragment()
+                }
+                R.id.navigation_explore -> {
+                    val exploreFragment = ExploreFragment()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.nav_host_fragment_activity_dashboard, exploreFragment).commit()
+                }
+                R.id.navigation_admissions -> {
+                    val admissionFragment = AdmissionFragment()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.nav_host_fragment_activity_dashboard, admissionFragment).commit()
+
+                    }
+                R.id.navigation_faqs -> {
+                    val faqsFragment = FAQsFragment()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.nav_host_fragment_activity_dashboard, faqsFragment).commit()
+                    supportFragmentManager.popBackStack()
+                    }
+            }
+        }
     }
 
+    private fun openMainFragment() {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.nav_host_fragment_activity_dashboard, fragment)
+        transaction.commit()
+    }
 }
